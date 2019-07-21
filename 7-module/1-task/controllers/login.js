347d26e64d1f9d550a3ced10bcfed5c908de921a
IGR <igrflex@gmail.com>
2019-07-21 19:58:19 +0300

    add solution for 7-module 1-task

--- 7-module/1-task/controllers/login.js
+++ 7-module/1-task/controllers/login.js
@@ -2,16 +2,16 @@ const uuid = require('uuid/v4');
 const passport = require('../libs/passport');
 
 module.exports.login = async function login(ctx, next) {
-  console.log('пришли в пост');
-  console.log('На входе ', ctx.body);
+  // console.log('пришли в пост');
+  // console.log('На входе ', ctx.body);
   await passport.authenticate('local', async (err, user, info) => {
     if (err) throw err;
 
-    console.log('err', err);
-    console.log('user', user);
-    console.log('info', info);
+    // console.log('err', err);
+    // console.log('user', user);
+    // console.log('info', info);
     if (!user) {
-      console.log('юзера нет');
+      // console.log('юзера нет');
 
       ctx.status = 400;
       ctx.body = {error: info};
--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -4,10 +4,10 @@ const User = require('../../models/User');
 module.exports = new LocalStrategy(
     {session: false},
     async function(email, password, done) {
-      console.log('пришли в стратегию локальную');
+      // console.log('пришли в стратегию локальную');
 
       try {
-        console.log('TRY ');
+        // console.log('TRY ');
         const user = await User.findOne({email: email});
         if (!user) return done(null, false, 'Нет такого пользователя');
 
@@ -18,7 +18,7 @@ module.exports = new LocalStrategy(
 
         return done(null, user);
       } catch (err) {
-        console.log(' ОШибка пришли в стратегию локальную');
+        // console.log(' ОШибка пришли в стратегию локальную');
 
         done(err);
       }
