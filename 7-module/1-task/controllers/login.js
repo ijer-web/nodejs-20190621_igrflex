d85177d417b1eebe8ddea93f0f837c1dfdc98bdd
IGR <igrflex@gmail.com>
2019-07-21 19:25:40 +0300

    add solution for 7-module 1-task

--- 7-module/1-task/controllers/login.js
+++ 7-module/1-task/controllers/login.js
@@ -2,10 +2,17 @@ const uuid = require('uuid/v4');
 const passport = require('../libs/passport');
 
 module.exports.login = async function login(ctx, next) {
+  console.log('пришли в пост');
+  console.log('На входе ', ctx.body);
   await passport.authenticate('local', async (err, user, info) => {
     if (err) throw err;
 
+    console.log('err', err);
+    console.log('user', user);
+    console.log('info', info);
     if (!user) {
+      console.log('юзера нет');
+
       ctx.status = 400;
       ctx.body = {error: info};
       return;
--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -3,7 +3,24 @@ const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      console.log('пришли в стратегию локальную');
+
+      try {
+        console.log('TRY ');
+        const user = await User.findOne({email: email});
+        if (!user) return done(null, false, 'Нет такого пользователя');
+
+        const isValidPass = await user.checkPassword(password);
+
+        if (!isValidPass) return done(null, false, 'Невереный пароль');
+
+
+        return done(null, user);
+      } catch (err) {
+        console.log(' ОШибка пришли в стратегию локальную');
+
+        done(err);
+      }
     }
 );
