309da97c1a5ce6a703a80704d175da6289e38f09
IGR <igrflex@gmail.com>
2019-07-24 21:56:08 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,35 +12,29 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
-      if (pathname.includes('/')) {
-        res.statusCode = 400;
-        res.end('Bad request');
-      };
-
-      fs.unlink(filepath, function(err) {
-        if (err) {
-          res.statusCode = 404;
-          res.end('No file such path');
-        }
-        res.statusCode = 200;
-        res.end('ok');
-      });
-
-      req.on('error', function(err) {
-        if (err) {
-          res.statusCode = 500;
-          res.end('*** internal error ***');
-        };
-      });
-
+      deleteHandler(req, res, pathname, filepath);
       break;
 
     default:
-
       res.statusCode = 501;
       res.end('Not implemented');
   }
 });
+
+function deleteHandler(req, res, pathname, filepath) {
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end();
+    return;
+  }
+
+  fs.unlink(filepath, (err) => {
+    if (err) {
+      res.statusCode = err.code === 'ENOENT' ? 404 : 500;
+    }
+
+    res.end();
+  });
+}
 
 module.exports = server;
