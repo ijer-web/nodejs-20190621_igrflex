be6a9b859b839ac8f1753ff5dbaf54732059009c
IGR <igrflex@gmail.com>
2019-07-30 20:19:05 +0300

    del timeout

--- 7-module/2-task/test/authenticate.test.js
+++ 7-module/2-task/test/authenticate.test.js
@@ -6,7 +6,6 @@ const users = require('../fixtures/users');
 
 describe('7-module-2-task', () => {
   describe('функция аутентификации', function() {
-    this.timeout(5000);
     before(async () => {
       await User.deleteMany();
 
